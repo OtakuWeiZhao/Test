dae55d64a6916efe88cb122adc7f25cfc58aaed7
OtakuWeiZhao <915009811@qq.com>
2017-01-20 22:06:31 +0800

    update code

---
+++ testDate/TestCase.java
@@ -0,0 +1,30 @@
+package testDate;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import org.junit.Test;
+
+public class TestCase {
+//	@Test
+//	public void testGetDate(){
+//		Date myDate = new Date();
+//		myDate.getTime();
+//		System.out.println(myDate);
+//		long time = myDate.getTime();
+//		System.out.println(time);
+//	}
+	Date myDate = new Date(1484374829);
+	@Test
+	public void timeStamp(){
+		myDate.getTime();
+		System.out.println(myDate);
+		Timestamp ts = new Timestamp(1484374829);
+		System.out.println(ts);
+		ts.setNanos(82900);
+		int myTimeStamp = ts.getNanos();
+		System.out.println(myTimeStamp);
+		String myTimeStamp2 = String.valueOf(myTimeStamp);
+		System.out.println(myTimeStamp2);
+	}
+}
